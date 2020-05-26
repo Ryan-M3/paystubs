45df2661e36a4a17392e65ea1d0c225288e9bb50
Ryan <gn341ram@gmail.com>
2020-05-25 21:14:39 -0700

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+setup(
+    name='paystubs',
+    version='1.0',
+    description='Personal Accounting CLI',
+    author='Ryan M',
+    author_email='gn341ram@gmail.com',
+    packages=['paystubs', 'paystubs/modules', 'paystubs/formatting', 'paystubs/financial_stmts', 'paystubs/data'],
+    url='',
+    ext_modules=[Extension('default_coa', ['default_coa.csv'])],
+
+)
